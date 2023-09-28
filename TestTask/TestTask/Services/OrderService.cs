d0d067fdfeab4963dfe78a53b6a3d463f9f77d88
VashStampedos <vlad-pashik@mail.ru>
2023-09-28 18:00:59 +0300

    TestTask

---
+++ TestTask/TestTask/Services/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class OrderService:IOrderService
+    {
+        ApplicationDbContext _dbContext;
+        public OrderService(ApplicationDbContext db)
+        {
+            this._dbContext = db;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            var orders = await _dbContext.Orders.AsNoTracking().Select(x => new
+            {
+                id = x.Id,
+                price = x.Price * x.Quantity,
+            }).ToListAsync();
+            var maxOrder = orders.MaxBy(x=> x.price);
+            var order = await _dbContext.Orders.FirstOrDefaultAsync(x => x.Id == maxOrder.id);
+           
+            return order;
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            var orders =await _dbContext.Orders.AsNoTracking().Where(x => x.Quantity > 10).ToListAsync();
+            return orders;
+        }
+    }
+}
---
+++ TestTask/TestTask/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class UserService:IUserService
+    {
+        
+        ApplicationDbContext _dbContext;
+        public UserService(ApplicationDbContext db) 
+        {
+            this._dbContext = db;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var ordersGroupedByUserId =await _dbContext.Orders.AsNoTracking().GroupBy(o => o.UserId).ToListAsync();
+            var user = ordersGroupedByUserId.MaxBy(x => x.Count());
+            var user1 =await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == user.Key);
+
+            return user1;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var users =await _dbContext.Users.AsNoTracking().Where(x => x.Status == UserStatus.Inactive).ToListAsync();
+           
+            return users;
+        }
+    }
+}
